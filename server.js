abc6c6c69eaea5d35b35c3a52d3d19006a50e913
James Mansour <jamestmansour@outlook.com>
2021-02-23 10:30:52 -0800

    Refactors to split the routs and functions.

--- server.js
+++ server.js
@@ -1,23 +1,38 @@
 'use strict';
-//add requrments===========================================================================================
+//=============add requrments===========================================================================================
 
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors')
 const app = express();
 
-//app config===========================================================================================
+//=============app config==============================================================================
 
 const PORT = process.env.PORT;
 app.use(cors())
 
-// routs===========================================================================================
+// =============routs===========================================================================================
 
-// return location info
-app.get('/location', (request, response) => {
+app.get('/location', handelLocation);
+app.get('/weather', handelWeather);
+
+
+function handelLocation(request, response) {
+    //get location data from file and serve up
     const locationData = require('./data/location.json')
     response.status(200).send(new Location(locationData[0], request.query.city))
-});
+}
+
+function handelWeather(request, response) {
+    //get location data from file and serve up
+    const weatherData = require('./data/weather.json')
+    //list of weather
+    const returnData = []
+    weatherData.data.forEach(day => {
+        returnData.push(new Weather(day))
+    })
+    response.status(200).send(returnData);
+}
 
 function Location(obj, city) {
     this.search_query = city,
@@ -26,26 +41,14 @@ function Location(obj, city) {
     this.longitude = obj.lon
 }
 
-//return weather info
-app.get('/weather', (request, response) => {
-    const weatherData = require('./data/weather.json')
-    //list of weather
-    const returnData = []
-    weatherData.data.forEach(day => {
-        returnData.push(new Weather(day))
-    })
-    response.status(200).send(returnData);
-});
-
 function Weather(obj) {
     this.forecast = obj.weather.description,
     this.time = obj.datetime
 }
 
 //catchall / 404
-
 app.use('*', (request, response) => response.send('Sorry, that route does not exist.'));
 
-//start app===========================================================================================
+//=============start app===========================================================================================
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
